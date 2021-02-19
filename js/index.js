d06fc71090da12f6559f72d25da5cbd75decc662
Marcel Vlasenco <marcelvlasenco@gmail.com>
2021-02-19 17:12:25 +0200

    Add input from start/end input field and the max-flow button works. Add functions to refresh the graph.

--- js/index.js
+++ js/index.js
@@ -1,77 +1,129 @@
 // Graph Nodes
 let nodes = [
-    {name: 's'},
-    {name: 't'},
-    {name: 'a'},
-    {name: 'b'},
-    {name: 'c'},
-    {name: 'd'}
-  ];
+  { index: 0, name: "s" },
+  { index: 1, name: "t" },
+  { index: 2, name: "a" },
+  { index: 3, name: "b" },
+  { index: 4, name: "c" },
+  { index: 5, name: "d" },
+];
 
-// Graph Links/Eadges 
+// Graph Links/Eadges
 let links = [
-    {source: 0, target: 2, capacity: 10, flow: 0 },
-    {source: 0, target: 3, capacity: 10, flow: 0 },
-    {source: 2, target: 3, capacity: 2, flow: 0 },
-    {source: 2, target: 4, capacity: 4, flow: 0 },
-    {source: 2, target: 5, capacity: 8, flow: 0 },
-    {source: 3, target: 5, capacity: 9, flow: 0 },
-    {source: 4, target: 1, capacity: 10, flow: 0 },
-    {source: 5, target: 4, capacity: 6, flow: 0 },
-    {source: 5, target: 1, capacity: 10, flow: 0 },
-  ];
+  { source: 0, target: 2, capacity: 10, flow: 0 },
+  { source: 0, target: 3, capacity: 10, flow: 0 },
+  { source: 2, target: 3, capacity: 2, flow: 0 },
+  { source: 2, target: 4, capacity: 4, flow: 0 },
+  { source: 2, target: 5, capacity: 8, flow: 0 },
+  { source: 3, target: 5, capacity: 9, flow: 0 },
+  { source: 4, target: 1, capacity: 10, flow: 0 },
+  { source: 5, target: 4, capacity: 6, flow: 0 },
+  { source: 5, target: 1, capacity: 10, flow: 0 },
+];
 
 // Sources and Targets of our flow, here can go any number of nodes
-let s = [0, 2];
-let t = [1, 4];
+let s = [];
+let t = [];
 
-// This part works only in browser console
-// It shows how my algorithm works for now
-console.log("The max flow of current graph is " + multiFord(links, nodes, s, t));
-console.log(links);
+// Adds nodes from DOM
+function addStartEndNodes() {
+  let start = document.getElementById("startData").value;
+  let end = document.getElementById("endData").value;
 
-//////// This part of code is just an example of graph visualization /////////
-// PS. I don't have a clue how D3.js library works
+  s = [];
+  t = [];
+
+  start = start.split(" ");
+  end = end.split(" ");
+
+  for (let i = 0; i < start.length; i++) {
+    for (let j = 0; j < nodes.length; j++) {
+      if (start[i] == nodes[j].name) {
+        s.push(j);
+      }
+    }
+  }
+
+  for (let i = 0; i < end.length; i++) {
+    for (let j = 0; j < nodes.length; j++) {
+      if (end[i] == nodes[j].name) {
+        t.push(j);
+      }
+    }
+  }
+}
+
+// Buttons functionality
+
+document.getElementById("maxFlowBtn").onclick = function () {
+  addStartEndNodes();
+  if (s.length != 0 && t.length != 0) {
+    alert("Max flow is " + multiFord(links, nodes, s, t));
+  } else {
+    alert("Add start and end nodes.");
+  }
+};
+
+//////////////////// D3.js ///////////////////
+
+// Iura daca stii vre-o metoda mai eficienta scrie
+
+// These variables are used by D3.js to plot the graph
+// These copies are created because D3.js changes some esential object proprieties and max flow algorithm won't work
+let d3links; // Here will be a copy of links
+let d3nodes; // Here will be a copy of nodes
+let link;
+let node;
+let svg;
+
+// Removes the existing graph
+function graphRemove() {
+  node.remove();
+  link.remove();
+  d3nodes = [];
+  d3links = [];
+}
+
+// Draws a new graph
+function graphInit() {
+  d3links = JSON.parse(JSON.stringify(links));
+  d3nodes = JSON.parse(JSON.stringify(nodes));
+
+  //////// This part of code is just an example of graph visualization /////////
+  // PS. I don't have a clue how D3.js library works
 
   //initilize svg or grab svg
-  var svg = d3.select("svg");
+  svg = d3.select("svg");
   var width = svg.attr("width");
   var height = svg.attr("height");
-  
 
   var simulation = d3
-    .forceSimulation(nodes)
-    .force(
-      "link",
-      d3
-        .forceLink()
-        .links(links)
-    )
-
+    .forceSimulation(d3nodes)
+    .force("link", d3.forceLink().links(d3links))
     .force("charge", d3.forceManyBody().strength(-30))
     .force("center", d3.forceCenter(width / 2, height / 2))
     .on("tick", ticked);
 
-  var link = svg
+  link = svg
     .append("g")
     .attr("class", "links")
     .selectAll("line")
-    .data(links)
+    .data(d3links)
     .enter()
     .append("line")
-    .attr("stroke-width", function(d) {
+    .attr("stroke-width", function (d) {
       return 3;
     });
 
-  var node = svg
+  node = svg
     .append("g")
     .attr("class", "nodes")
     .selectAll("circle")
-    .data(nodes)
+    .data(d3nodes)
     .enter()
     .append("circle")
     .attr("r", 5)
-    .attr("fill", function(d) {
+    .attr("fill", function (d) {
       return "red";
     })
     .call(
@@ -84,24 +136,24 @@ console.log(links);
 
   function ticked() {
     link
-      .attr("x1", function(d) {
+      .attr("x1", function (d) {
         return d.source.x;
       })
-      .attr("y1", function(d) {
+      .attr("y1", function (d) {
         return d.source.y;
       })
-      .attr("x2", function(d) {
+      .attr("x2", function (d) {
         return d.target.x;
       })
-      .attr("y2", function(d) {
+      .attr("y2", function (d) {
         return d.target.y;
       });
 
     node
-      .attr("cx", function(d) {
+      .attr("cx", function (d) {
         return d.x;
       })
-      .attr("cy", function(d) {
+      .attr("cy", function (d) {
         return d.y;
       });
   }
@@ -122,3 +174,20 @@ console.log(links);
     d.fx = null;
     d.fy = null;
   }
+}
+
+//////////// TEST CASE ///////////////
+// The graph will dissapear and apear
+
+graphInit();
+
+setTimeout(function () {
+  graphRemove();
+}, 2000);
+
+setTimeout(function () {
+  graphInit();
+}, 4000);
+
+// You can add start and end nodes to calculate the max flow
+// The nodes are declared by their names and are separated by one space
