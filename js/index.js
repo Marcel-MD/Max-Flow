afaca0530532da7449bd8612a2908383c4b208ed
Marcel Vlasenco <marcelvlasenco@gmail.com>
2021-02-23 21:27:50 +0200

    Add some comments and format the source files

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/////////    Our Graph /////////
+
 // Graph Nodes
 let nodes = [
   { name: "s" },
@@ -24,6 +26,8 @@ let links = [
 // Sources and Targets of our flow, here can go any number of nodes
 let s = [];
 let t = [];
+
+///////////    Text area / Editor input    //////////////
 
 // Adds start end nodes from DOM
 function addStartEndNodes() {
@@ -53,8 +57,7 @@ function addStartEndNodes() {
   }
 }
 
-// Text area / Editor input
-
+// Adds a new edge
 function addEdge(s, t, c) {
   for (let i = 0; i < links.length; i++) {
     if (links[i].source === s && links[i].target === t) {
@@ -65,17 +68,18 @@ function addEdge(s, t, c) {
   links.push({ source: s, target: t, capacity: c, flow: 0 });
 }
 
+// Gets the node index if it exists else a new node is created
 function getNode(name) {
   for (let i = 0; i < nodes.length; i++) {
     if (nodes[i].name == name) {
       return i;
     }
   }
-
   nodes.push({ name: name });
   return nodes.length - 1;
 }
 
+// Gets the text from editor and passes it to the required functions
 function getText() {
   let lines = document.getElementById("textAreaID").value.split("\n");
   let edgeData;
@@ -85,8 +89,8 @@ function getText() {
     console.log(edgeData);
     if (edgeData.length >= 3) {
       let c = parseInt(edgeData[2], 10);
-      if(isNaN(c)) {
-        alert("Invalid edge data on line " + (i+1));
+      if (isNaN(c)) {
+        alert("Invalid edge data on line " + (i + 1));
       } else {
         let s = getNode(edgeData[0]);
         let t = getNode(edgeData[1]);
@@ -96,15 +100,15 @@ function getText() {
   }
 }
 
-// Buttons functionality
+////////   Buttons functionality    ///////////
 
-var result = document.getElementById('result');
+var result = document.getElementById("result");
 
 // Max Flow Button
 document.getElementById("maxFlowBtn").onclick = function () {
   addStartEndNodes();
   if (s.length != 0 && t.length != 0) {
-    result.innerHTML = ("Maximum flow is " + multiFord(links, nodes, s, t));
+    result.innerHTML = "Maximum flow is " + multiFord(links, nodes, s, t);
   } else {
     alert("Add valid start and end nodes.");
   }
@@ -112,14 +116,14 @@ document.getElementById("maxFlowBtn").onclick = function () {
   graphInit();
 };
 
-// Add Edge
+// Add Edge Button
 document.getElementById("addEdgesBtn").onclick = function () {
   getText();
   graphRemove();
   graphInit();
 };
 
-// Delete Graph
+// Delete Graph Button
 document.getElementById("deleteGraphBtn").onclick = function () {
   nodes = [];
   links = [];
@@ -127,7 +131,7 @@ document.getElementById("deleteGraphBtn").onclick = function () {
   graphInit();
 };
 
-//////////////////// D3.js ///////////////////
+///////////////    D3.js    ////////////////
 
 // These variables are used by D3.js to plot the graph
 // These copies are created because D3.js changes some esential object proprieties and max flow algorithm won't work
@@ -149,40 +153,42 @@ function graphInit() {
 
   svg = d3.select("svg");
   var colors = d3.scaleOrdinal(d3.schemeCategory10),
-      width = +svg.attr("width"),
-      height = +svg.attr("height"),
-      node,
-      link,
-      edgepaths,
-      edgelabels;
-  
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    node,
+    link,
+    edgepaths,
+    edgelabels;
+
   svg
-    .append('defs')
-    .append('marker')
-    .attr('id','arrowhead')
-    .attr('viewBox','-0 -5 10 10')
-    .attr('refX', 13)
-    .attr('refY', 0 )
-    .attr('orient','auto')
-    .attr('markerWidth', 3 )
-    .attr('markerHeight', 3 )
-    .attr('xoverflow','visible')
-    .append('svg:path')
-    .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-    .attr('fill', '#999')
+    .append("defs")
+    .append("marker")
+    .attr("id", "arrowhead")
+    .attr("viewBox", "-0 -5 10 10")
+    .attr("refX", 13)
+    .attr("refY", 0)
+    .attr("orient", "auto")
+    .attr("markerWidth", 3)
+    .attr("markerHeight", 3)
+    .attr("xoverflow", "visible")
+    .append("svg:path")
+    .attr("d", "M 0,-5 L 10 ,0 L 0,5")
+    .attr("fill", "#999")
     .attr("fill-opacity", "0.8")
-    .style('stroke','none');
-  
-  var simulation = d3.forceSimulation(d3nodes) 
-  .force("link", d3.forceLink().links(d3links))//.distance(150).strength(1))
-  .force("charge", d3.forceManyBody().strength(-1000))
+    .style("stroke", "none");
+
+  var simulation = d3
+    .forceSimulation(d3nodes)
+    .force("link", d3.forceLink().links(d3links)) //.distance(150).strength(1))
+    .force("charge", d3.forceManyBody().strength(-1000))
     .force("center", d3.forceCenter(width / 2, height / 2));
-  
+
   update(d3links, d3nodes);
-  
+
   // ---
   function update(links, nodes) {
-    link = svg.selectAll(".link")
+    link = svg
+      .selectAll(".link")
       .data(links)
       .enter()
       .append("line")
@@ -190,97 +196,95 @@ function graphInit() {
       .attr("stroke", "lightgray")
       .attr("stroke-opacity", "0.5")
       .attr("class", "link")
-      .attr('marker-end','url(#arrowhead)')
-  
-    link
-      .append("title")
-      .text(function(d) {
-        if (d.flow) {return d.flow + " / " + d.capacity;}
-        else {return 0 + " / " + d.capacity;}
-      });
-  
-    edgepaths = svg.selectAll(".edgepath")
+      .attr("marker-end", "url(#arrowhead)");
+
+    link.append("title").text(function (d) {
+      if (d.flow) {
+        return d.flow + " / " + d.capacity;
+      } else {
+        return 0 + " / " + d.capacity;
+      }
+    });
+
+    edgepaths = svg
+      .selectAll(".edgepath")
       .data(links)
       .enter()
-      .append('path')
-      .attr("class", 'edgepath')
-      .attr('fill-opacity', 0)
-      .attr('stroke-opacity', 0)
-      .attr('id', (d, i) => 'edgepath' + i)
+      .append("path")
+      .attr("class", "edgepath")
+      .attr("fill-opacity", 0)
+      .attr("stroke-opacity", 0)
+      .attr("id", (d, i) => "edgepath" + i)
       .style("pointer-events", "none");
-  
-    edgelabels = svg.selectAll(".edgelabel")
+
+    edgelabels = svg
+      .selectAll(".edgelabel")
       .data(links)
       .enter()
-      .append('text')
+      .append("text")
       .style("pointer-events", "none")
-      .attr('class','edgelabel')
-      .attr('id',(d, i) => 'edgelabel' + i)
+      .attr("class", "edgelabel")
+      .attr("id", (d, i) => "edgelabel" + i)
       .attr("writing-mode", "vertical-rl")
-      .attr('font-size', 11)
-      .attr('fill', 'white');
-  
-    edgelabels.append('textPath')
-      .attr('xlink:href', (d, i) =>'#edgepath' + i)
+      .attr("font-size", 11)
+      .attr("fill", "white");
+
+    edgelabels
+      .append("textPath")
+      .attr("xlink:href", (d, i) => "#edgepath" + i)
       .style("text-anchor", "middle")
       .style("pointer-events", "none")
       .attr("startOffset", "50%")
-      .text(function(d) {
-        if (d.flow) {return d.flow + " / " + d.capacity;}
-        else {return 0 + " / " + d.capacity;}
+      .text(function (d) {
+        if (d.flow) {
+          return d.flow + " / " + d.capacity;
+        } else {
+          return 0 + " / " + d.capacity;
+        }
       });
-  
-    node = svg.selectAll(".node")
+
+    node = svg
+      .selectAll(".node")
       .data(nodes)
       .enter()
       .append("g")
-      .attr("class", "node")
-  
-    node.append("circle")
+      .attr("class", "node");
+
+    node
+      .append("circle")
       .attr("r", 7)
       .style("fill", (d, i) => colors(i))
       .attr("stroke", "white")
       .attr("stroke-width", "2px");
-  
-    node.append("title")
-      .text((d) => d.name);
-  
-    node.append("text")
+
+    node.append("title").text((d) => d.name);
+
+    node
+      .append("text")
       .attr("dy", -10)
       .text((d) => d.name)
-      .attr('fill', 'white');
-  
-    simulation
-      .nodes(nodes)
-      .on("tick", ticked);
-  
-    simulation.force("link")
-      .links(links);
+      .attr("fill", "white");
+
+    simulation.nodes(nodes).on("tick", ticked);
+
+    simulation.force("link").links(links);
   }
-  
+
   function ticked() {
     link
       .attr("x1", (d) => d.source.x)
       .attr("y1", (d) => d.source.y)
       .attr("x2", (d) => d.target.x)
       .attr("y2", (d) => d.target.y);
-  
-    node
-      .attr("transform", (d) => "translate(" + d.x + ", " + d.y + ")");
-  
-    edgepaths.attr('d', (d) => (
-      `M ${d.source.x} ${d.source.y} L ${d.target.x} ${d.target.y}`
-    ));
+
+    node.attr("transform", (d) => "translate(" + d.x + ", " + d.y + ")");
+
+    edgepaths.attr(
+      "d",
+      (d) => `M ${d.source.x} ${d.source.y} L ${d.target.x} ${d.target.y}`
+    );
   }
 }
 
 // The firs graph render
 graphInit();
-
-//////////// TEST CASE ///////////////
-// All the input fields and buttons:
-// Maximum Flow
-// Add Edges
-// Delete Graph
-
-// Are fully functional but without data validation
